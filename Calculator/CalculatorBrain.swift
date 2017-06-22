835a27afde4fd15a412dc208c2dd358c399ed37d
Michael Jordan <michael.gary.jordan@icloud.com>
2017-06-22 14:38:48 -0600

    Added nullary function to calculator brain. Added nullary random number generator function to the calculator.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+func randomDouble() -> Double {
+    return Double(arc4random_uniform(UInt32.max)) / Double(UInt32.max)
+}
+
 struct CalculatorBrain {
     
     var numberFormatter: NumberFormatter?
@@ -29,29 +33,31 @@ struct CalculatorBrain {
     
     private enum Operation {
         case constant(Double)
+        case nullaryOperation(() -> Double)
         case unaryOperation((Double) -> Double)
         case binaryOperation((Double, Double) -> Double)
         case equals
     }
     
     private var operations: Dictionary<String, Operation> = [
-        "π" : Operation.constant(Double.pi),
-        "e" : Operation.constant(M_E),
-        "√" : Operation.unaryOperation(sqrt),
-        "∛" : Operation.unaryOperation({pow($0, 1.0/3.0)}),     // Cubed root function
-        "x²" : Operation.unaryOperation({$0 * $0}),
-        "x³" : Operation.unaryOperation({$0 * $0 * $0}),
+        "π"   : Operation.constant(Double.pi),
+        "e"   : Operation.constant(M_E),
+        "RND" : Operation.nullaryOperation(randomDouble),
+        "√"   : Operation.unaryOperation(sqrt),
+        "∛"   : Operation.unaryOperation({pow($0, 1.0/3.0)}),     // Cubed root function
+        "x²"  : Operation.unaryOperation({$0 * $0}),
+        "x³"  : Operation.unaryOperation({$0 * $0 * $0}),
         "sin" : Operation.unaryOperation(sin),
         "cos" : Operation.unaryOperation(cos),
         "tan" : Operation.unaryOperation(tan),
         "log" : Operation.unaryOperation(log10),
-        "ln" : Operation.unaryOperation(log),
-        "±" : Operation.unaryOperation({-$0}),
-        "×" : Operation.binaryOperation({$0 * $1}),
-        "÷" : Operation.binaryOperation({$0 / $1}),
-        "+" : Operation.binaryOperation({$0 + $1}),
-        "−" : Operation.binaryOperation({$0 - $1}),
-        "=" : Operation.equals
+        "ln"  : Operation.unaryOperation(log),
+        "±"   : Operation.unaryOperation({-$0}),
+        "×"   : Operation.binaryOperation({$0 * $1}),
+        "÷"   : Operation.binaryOperation({$0 / $1}),
+        "+"   : Operation.binaryOperation({$0 + $1}),
+        "−"   : Operation.binaryOperation({$0 - $1}),
+        "="   : Operation.equals
     ]
     
     mutating func performOperation(_ symbol: String) {
@@ -59,6 +65,9 @@ struct CalculatorBrain {
             switch operation {
             case .constant(let value):
                 accumulator = (value, "\(symbol)")
+            case .nullaryOperation(let function):
+                let result = function()
+                accumulator = (result, formatValue(result))
             case .unaryOperation(let function):
                 if accumulator.value != nil {
                     let description : String
